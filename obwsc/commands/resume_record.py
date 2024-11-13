ee855879e9859fa72b224e9466220b4b4e3ff59b
YoWiDiN <yowidin@gmail.com>
2024-11-13 09:17:55 +0100

    Restart macOS capture upon resumption
    This way the new (non-deprecated) capture will be automatically fixed
    in case the screen was previously locked.

--- obwsc/commands/resume_record.py
+++ obwsc/commands/resume_record.py
@@ -1,6 +1,7 @@
 from obwsc.commands.event_based_command import EventBasedCommand
+from obsws_python.error import OBSSDKRequestError
 
-import logging
+import sys
 
 from obwsc.log import Log
 
@@ -22,6 +23,21 @@ class ResumeRecord(EventBasedCommand):
         if event.output_state == 'OBS_WEBSOCKET_OUTPUT_RESUMED':
             self.done.set()
 
+    def _restart_macos_inputs(self):
+        # On macOS the "screen capture" inputs get "broken" after locking the screen, but luckily OBS provides a button
+        # for restarting the capture.
+        # In this function we are iterating over all inputs, where this button is present and ask OBS to press it.
+        if sys.platform != 'darwin':
+            Log.debug(f'Skipping screen capture restarts: not on macOS: {sys.platform}')
+            return
+
+        resp = self.ws.get_input_list('screen_capture')
+        for capture in resp.inputs:
+            try:
+                self.ws.press_input_properties_button(capture['inputName'], "reactivate_capture")
+            except OBSSDKRequestError:
+                Log.debug(f'Screen capture reactivation failed for "{capture["inputName"]}"')
+
     def execute(self):
         status = self.ws.get_record_status()
         if not status.output_active:
@@ -30,7 +46,9 @@ class ResumeRecord(EventBasedCommand):
         if not status.output_paused:
             Log.debug('Recording is already running')
             self.done.set()
-            return
+        else:
+            self.ws.resume_record()
 
-        self.ws.resume_record()
         self.done.wait()
+
+        self._restart_macos_inputs()
